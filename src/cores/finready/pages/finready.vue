15c32a97d2c2f253e71123058893e2037bb5f546
Teerachai Laothong <teerachai@finsoft2023.com>
2024-10-12 09:55:11 +0700

    Add facingweb and finready

---
+++ src/cores/finready/pages/finready.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>FinReady</h1>
+    <h2>FinReady</h2>
+  </div>
+</template>
---
+++ src/cores/finready/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Index Page</h1>
+    <h2>FinReady</h2>
+  </div>
+</template>
